24af765f33ee36063d1129bb1825b3d58b801986
Ivan Sopov <sopov.ivan@gmail.com>
2013-12-22 18:57:28 +0200

    bumping spring-boot and undertow versions

--- src/main/java/com/sopovs/moradanen/spring/boot/autoconfigure/web/UndertowEmbeddedServletContainer.java
+++ src/main/java/com/sopovs/moradanen/spring/boot/autoconfigure/web/UndertowEmbeddedServletContainer.java
@@ -2,27 +2,40 @@ package com.sopovs.moradanen.spring.boot.autoconfigure.web;
 
 import io.undertow.Undertow;
 
+import java.util.List;
+
 import org.springframework.boot.context.embedded.EmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerException;
+import org.springframework.util.ReflectionUtils;
 
 public class UndertowEmbeddedServletContainer implements EmbeddedServletContainer {
 
-	private final Undertow undertow;
+    private final Undertow undertow;
 
-	public UndertowEmbeddedServletContainer(Undertow undertow) {
-		this.undertow = undertow;
-	}
+    public UndertowEmbeddedServletContainer(Undertow undertow) {
+        this.undertow = undertow;
+    }
 
-	@Override
-	public void start() throws EmbeddedServletContainerException {
-		undertow.start();
+    @Override
+    public void start() throws EmbeddedServletContainerException {
+        undertow.start();
 
-	}
+    }
 
-	@Override
-	public void stop() throws EmbeddedServletContainerException {
-		undertow.stop();
+    @Override
+    public void stop() throws EmbeddedServletContainerException {
+        undertow.stop();
+    }
 
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public int getPort() {
+        List<Object> listeners = (List<Object>) ReflectionUtils.getField(
+                ReflectionUtils.findField(Undertow.class, "listeners"), undertow);
+        for (Object listener : listeners) {
+            return (Integer) ReflectionUtils.getField(ReflectionUtils.findField(listener.getClass(), "port"), listener);
+        }
 
+        return 0;
+    }
 }
