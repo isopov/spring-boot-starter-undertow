e56f0316a5cc725f8cb471e4185705fa7fb112e5
Ivan Sopov <sopov.ivan@gmail.com>
2014-02-14 01:09:37 +0200

    test for custom errorPage defined via errorCode 404 that does work and comment regarding another issue for Undertow

--- src/test/java/com/sopovs/moradanen/spring/boot/autoconfigure/web/UndertowEmbeddedServletContainerFactoryTests.java
+++ src/test/java/com/sopovs/moradanen/spring/boot/autoconfigure/web/UndertowEmbeddedServletContainerFactoryTests.java
@@ -1,8 +1,16 @@
 package com.sopovs.moradanen.spring.boot.autoconfigure.web;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.boot.context.embedded.AbstractEmbeddedServletContainerFactoryTests;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.context.embedded.ExampleServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.http.HttpStatus;
 
 public class UndertowEmbeddedServletContainerFactoryTests extends
         AbstractEmbeddedServletContainerFactoryTests
@@ -20,6 +28,25 @@ public class UndertowEmbeddedServletContainerFactoryTests extends
     // Wait for the https://issues.jboss.org/browse/UNDERTOW-192
     public void errorPage() throws Exception {
         super.errorPage();
+    }
+
+    @Override
+    @Ignore
+    @Test
+    // Wait for the https://issues.jboss.org/browse/UNDERTOW-193
+    public void specificContextRoot() throws Exception {
+        super.specificContextRoot();
+    }
+
+    @Test
+    public void errorPage404() throws Exception {
+        ConfigurableEmbeddedServletContainerFactory factory = getFactory();
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/hello"));
+        this.container = factory.getEmbeddedServletContainer(
+                new ServletRegistrationBean(new ExampleServlet(), "/hello"));
+        this.container.start();
+        assertThat(getResponse("http://localhost:8080/hello"), equalTo("Hello World"));
+        assertThat(getResponse("http://localhost:8080/not-found"), equalTo("Hello World"));
     }
 
     @Override
@@ -44,14 +71,6 @@ public class UndertowEmbeddedServletContainerFactoryTests extends
     public void loadOnStartAfterContextIsInitialized() throws Exception {
         // TODO
         super.loadOnStartAfterContextIsInitialized();
-    }
-
-    @Override
-    @Ignore
-    @Test
-    public void specificContextRoot() throws Exception {
-        // TODO
-        super.specificContextRoot();
     }
 
 }
